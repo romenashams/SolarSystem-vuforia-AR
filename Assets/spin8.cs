e014a697f2e273e0fc96570be434c25fc8ba45b0
Anita Tehrani <117678324+anitatehrani@users.noreply.github.com>
2024-05-14 02:20:14 +0200

    'update'

---
+++ Assets/spin8.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spin8 : MonoBehaviour
+{   const float ROTATION_SPEED = -62;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var parentTransform = transform.parent;
+        transform.RotateAround(parentTransform.position, new Vector3(0.0f, 0.0f,1.0f), ROTATION_SPEED* Time.deltaTime);
+    }
+}
